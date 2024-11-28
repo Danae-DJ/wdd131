5093264119df80862831554a2aa0973c1a78891a
Danae-DJ <danae_taiyaki@hotmail.com>
2024-11-27 22:55:08 -0600

    added changes

--- week04/scripts/courses.js
+++ week04/scripts/courses.js
@@ -3,3 +3,7 @@ const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
   };
+
+let studentCourse1 = { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'};
+
+let studentCourse2 = { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'};
