21202f009a62c794834145f31646540ecf18389f
Danae-DJ <danae_taiyaki@hotmail.com>
2024-12-08 02:13:15 -0600

    added changes

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,7 @@
+// Update the footer year
+const yearSpan = document.getElementById('currentYear');
+yearSpan.textContent = new Date().getFullYear();
+
+// Update the last modified date
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
