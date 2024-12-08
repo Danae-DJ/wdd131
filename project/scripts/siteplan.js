e51438ef80ab21a6ad003ccd7b01f1b29b716935
Danae-DJ <danae_taiyaki@hotmail.com>
2024-12-08 02:39:30 -0600

    added changes

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -5,3 +5,4 @@ yearSpan.textContent = new Date().getFullYear();
 // Update the last modified date
 const lastModified = document.getElementById('lastModified');
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
