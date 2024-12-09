e357ea48390e90f5030f2e3f946a30e7bd8a16e5
Danae-DJ <danae_taiyaki@hotmail.com>
2024-12-09 00:07:36 -0600

    added codes

---
+++ scripts/form.js
@@ -0,0 +1,4 @@
+//footer date
+const lastModDate = new Date(document.lastModified);
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById("lastModified").textContent = "Last Modified: " + lastModDate.toLocaleDateString('en-US', options);
